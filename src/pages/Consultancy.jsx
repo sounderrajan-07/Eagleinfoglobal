01b17271d61ea40ea0f412f8d4677cfa430589b5
Sounderrajan S <soundhersenthil07@gmail.com>
2026-06-26 13:16:27 +0530

    Update Consultancy.jsx

--- src/pages/Consultancy.jsx
+++ src/pages/Consultancy.jsx
@@ -104,7 +104,7 @@ function Consultancy() {
       image: "/images/Consultancy/coat.jpg"
     },
     {
-      name: "Selvam",
+      name: "Siva kumar",
       role: "Legal Advisor",
       expertise: "Madras High Court",
       experience: "18+ years",
